569fab948afa0620031377917114f341d781f741
kaitlynlee31 <kaitlynlee@ucsb.edu>
2024-05-01 16:47:55 -0700

    created data cleaning functions, incorporated 2023 sightings data

---
+++ data-cleaning.R
@@ -0,0 +1,129 @@
+###########################
+# WHALE:
+# clean whale dataset:
+clean_whale <- function(dataset){
+  cleaned = subset(dataset, DecLat < 39)  # restrict latitude values
+  cleaned = subset(dataset, DecLong < -113)  # restrict longitude values
+  cleaned = cleaned[-1105,]
+  cleaned = cleaned[-2064,]  # removing inland bottlenose dolphin sighting 
+  cleaned <- cleaned %>% 
+    mutate(Year = as.integer(paste("20", substr(Cruise, 3, 4), sep = "")),  # create Year col
+           Month = month.name[as.integer(substr(Cruise, 5, 6))]) %>%  # create Month col
+    mutate(Cruise = paste(Month, Year)) %>%  # rename cruises
+    mutate(Season = trimws(Season)) %>%   # trim whitespace for Season col
+    select(Cruise, Season, Year, DateTimeLocal, DecLat, DecLong, Best, Min, Max, Calf, 
+           Species1, Species2, SpeciesName, SubOrder)
+  return(cleaned)
+}
+
+
+# clean raw data for whale:
+species_code <- read.csv("data/CalCOFI_species_codes.csv")
+clean_new_sightings <- function(new_sightings){
+  cleaned <- new_sightings %>% 
+    filter(EID != 'EID') %>%  # drop excess rows
+    filter(ev == 'SIT' | ev == 'UPD') %>%  # only include sightings data
+    filter(X1 == 'CETA') %>%   # only cetacean sightings
+    select(cruise, 
+           when, 
+           Y, 
+           X, 
+           port, 
+           star, 
+           X13,
+           X14,
+           X15,
+           X16,
+           X17,
+           X19
+    ) %>% 
+    rename(DecLat = Y,
+           DecLong = X,
+           PortObs = port,
+           StbdObs = star,
+           Min = X14,
+           Max = X15,
+           Calf = X16
+    ) %>% 
+    mutate(Year = as.integer(substr(when, 1, 4)),  # create Year col
+           Month = month.name[as.integer(substr(when, 6, 7))]) %>%  # create Month col
+    mutate(Cruise = paste(Month, Year),
+           Season = as.character(sapply(as.integer(substr(when, 6, 7)), get_season)),
+           when = as.POSIXct(when, format = "%Y-%m-%d %H:%M:%S"),
+           DateTimeLocal = format(when, "%m/%d/%Y %H:%M"),
+           Best = as.integer(X13),
+           Species1 = str_to_title(trimws(X17)),
+           Species2 = str_to_title(X19)
+    ) %>% 
+    left_join(species_code, by = c("Species1" = "speciesCode")) %>% 
+    rename(SpeciesName = speciesName,
+           SubOrder = taxonomy) %>% 
+    select(Cruise, Season, Year, DateTimeLocal, DecLat, DecLong, Best, Min, Max, Calf, 
+           Species1, Species2, SpeciesName, SubOrder)
+  
+  return(cleaned)
+}
+
+
+###########################
+# EDNA:
+clean_edna <- function(data){
+  cleaned <- data %>% 
+    mutate(Cruise = paste(month.name[as.integer(substr(cruise, 5, 6))], year))
+  colnames(cleaned)[colnames(cleaned) == "year"] ="Year"
+  
+  return(cleaned)
+}
+
+###########################
+# VIZ:
+clean_viz <- function(data){
+  cleaned <- data %>% mutate(Cruise = paste(month.name[as.integer(substr(Cruise, 8, 9))], 
+                                                 substr(Cruise, 3, 6)))
+  return(cleaned)
+}
+
+###########################
+# ACOUSTIC:
+clean_acoustic <- function(data){
+  cleaned <- data %>% 
+    mutate(Cruise = paste(month.name[as.integer(substr(Cruise, 5, 6))], Year))
+  return(cleaned)
+}
+
+
+
+############################
+
+# OTHER FUNCTIONS:
+## Defining a function to convert month number to season
+get_season <- function(month) {
+  if (month %in% c(3,4,5)) {
+    return("spring")
+  } else if (month %in% c(06, 07, 08)) {
+    return("summer")
+  } else if (month %in% c(09, 10, 11)) {
+    return("fall")
+  } else if (month %in% c(12, 01, 02)) {
+    return("winter")
+  }
+}
+
+
+## function to scale the dots:
+adjustSize <- function(value) {
+  if (!is.na(value)){
+    if (value < 10) {
+      return(4750 + (1000 * log(value)))
+    } 
+    else {
+      return((log(value) * 2000) + 3750)
+      
+    }
+  }
+}
+
+## normalize effort function:
+normalize_effort <- function(x, na.rm = TRUE) {
+  return((x- min(x)) /(max(x)-min(x)))
+}
